bd6d15c67f0f7a4284a148df4a36d2980eb945e5
Nils Büchner <nils.buechner@wiit.cloud>
2024-04-03 03:31:18 +0200

    update latest.sh to auto commit and release

--- latest.sh
+++ latest.sh
@@ -13,6 +13,20 @@ if [[ "$VERSION" != "$LOCAL_VERSION" ]]; then
    sed -i "/^\([[:space:]]*version: \).*/s//\1$VERSION/" snapcraft.yaml
    echo "git commit -a -m 'version update: $VERSION'"
    echo "git push"
+   git commit -a -m 'version update: $VERSION'
+   git push
+   output=$(snapcraft)
+
+   # Use grep to find the line containing the created file name, then use awk to extract the file name
+   filename=$(echo "$output" | grep 'Created snap package' | awk '{print $4}')
+
+   # Check if filename was extracted
+   if [ -n "$filename" ]; then
+       echo "Created file: $filename"
+       snapcraft upload --release=stable $filename && rm -f $filename
+   else
+       echo "Failed to extract file name from snapcraft output."
+   fi
 else
    echo "no update available. current version is $VERSION"
 fi
